9e5f17cf2972036c737e68969e89f87a7f06681a
t9md <taqumd@gmail.com>
2010-11-11 09:49:57 +0900

    add run_file command

--- README.md
+++ README.md
@@ -31,18 +31,26 @@ This script is for `iTerm` and have very limited feature compared to terminitor.
 This is sample setting. This DSL is very very similar to [ terminitor ]( https://github.com/achiu/terminitor ). 
 Yes, I created this because `iTerm` was not supported by `terminitor`.
 
-    window :name => "tab1", :win_bounds => {:side => 0, :top => 0, :width => 500, :height => 500 } do
+    window :name => "win1 tab1", :win_bounds => {:side => 0, :top => 0, :width => 500, :height => 500 } do
+      run "echo 'win1 tab1'"
+
       tab :name => "tab2", :session => "Default Session" do
-        run "echo 'tab2'"
+        # You can run command from file. comment-in followin line to see.
+        # run_file "~/.iterminitor/run_file_example"
+        run 'echo tab2'
       end
 
-      tab :name => "tab3", :session => "Bookmarked Session 1" do
+      # You can specify bookmarked session name for ':session' option
+      # ex) tab :name => "tab3", :session => "Bookmarked Session1" do
+      tab :name => "tab3", :session => "Default Session" do
         run "echo 'tab3'"
       end
     end
 
-    window :name => "tab1", :win_bounds => {:side => 500, :top => 0, :width => 500, :height => 500 } do
-      tab :name => "tab2", :session => "Bookmarked Session2" do
+    window :name => "win2 tab1", :win_bounds => {:side => 500, :top => 0, :width => 500, :height => 500 } do
+      run "echo 'win2 tab1'"
+
+      tab :name => "tab2", :session => "Default Session" do
         run "echo 'tab2'"
       end
 
--- iTerminitor.rb
+++ iTerminitor.rb
@@ -28,17 +28,25 @@ class ITerminitor
       Dir.mkdir(@@setting_dir)
       File.open(@@setting_dir + "/sample.rb", 'w') do |f|
         f.puts <<EOS
-window :name => "tab1", :win_bounds => {:side => 0, :top => 0, :width => 500, :height => 500 } do
+window :name => "win1 tab1", :win_bounds => {:side => 0, :top => 0, :width => 500, :height => 500 } do
+  run "echo 'win1 tab1'"
+
   tab :name => "tab2", :session => "Default Session" do
-    run "echo 'tab2'"
+    # You can run command from file. comment-in followin line to see.
+    # run_file "~/.iterminitor/run_file_example"
+    run 'echo tab2'
   end
 
+  # You can specify bookmarked session name for ':session' option
+  # ex) tab :name => "tab3", :session => "Bookmarked Session1" do
   tab :name => "tab3", :session => "Default Session" do
     run "echo 'tab3'"
   end
 end
 
-window :name => "tab1", :win_bounds => {:side => 500, :top => 0, :width => 500, :height => 500 } do
+window :name => "win2 tab1", :win_bounds => {:side => 500, :top => 0, :width => 500, :height => 500 } do
+  run "echo 'win2 tab1'"
+
   tab :name => "tab2", :session => "Default Session" do
     run "echo 'tab2'"
   end
@@ -49,6 +57,21 @@ window :name => "tab1", :win_bounds => {:side => 500, :top => 0, :width => 500, 
 end
 EOS
       end
+      File.open(@@setting_dir + "/run_file_example", 'w') do |f|
+        f.puts "ls -l\necho 'run_file_sample'"
+      end
+      puts
+      puts "[created] #{@@setting_dir}/sample.rb"
+      puts "[created] #{@@setting_dir}/run_file_sample"
+      puts
+      puts "Check setting DSL"
+      puts " $ cat #{@@setting_dir}/sample.rb"
+      puts
+      puts "Try sample config"
+      puts " $ #{$PROGRAM_NAME} start sample.rb"
+      puts
+      puts "   Thanks."
+      puts
     end
   end
 
@@ -83,6 +106,14 @@ EOS
     @term.current_terminal.current_session.write(:text => cmd)
   end
 
+
+  def run_file(file)
+    file = File.expand_path(file)
+    unless File.readable? file
+      raise "File #{file} not found!"
+    end
+    @term.current_terminal.current_session.write(:contents_of_file => file)
+  end
 
   def window_bounds(opt)
     s = opt[:side]
